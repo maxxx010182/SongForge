26cb5b45c782b7e173650653dacca318edfde8f8
Maxim Moshkin <maxxx010182@gmail.com>
2026-09-16 06:25:17 +0300

    2.11.81 FAQ ~3 min, 1/5 note modal opens the right package

--- scripts/deploy-local.sh
+++ scripts/deploy-local.sh
@@ -4,7 +4,7 @@ set -e
 
 DIR="${HOME}/SongForge"
 ARCHIVE="${1:-/tmp/songforge-update.tar.gz}"
-EXPECTED_VERSION="2.11.80"
+EXPECTED_VERSION="2.11.81"
 
 strip_crlf() {
   local f="$1"
--- scripts/deploy-vps.sh
+++ scripts/deploy-vps.sh
@@ -6,7 +6,7 @@
 set -e
 
 DIR="${HOME}/SongForge"
-EXPECTED_VERSION="2.11.80"
+EXPECTED_VERSION="2.11.81"
 ARCHIVE_URL="https://codeload.github.com/maxxx010182/SongForge/tar.gz/main"
 
 strip_crlf() {
